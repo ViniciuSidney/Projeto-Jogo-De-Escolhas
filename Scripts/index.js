1ff488a1c6c4f8cfd09830338ad86a523368c8ec
Vinícius <viniciusidney0@gmail.com>
2024-11-21 22:05:03 -0300

    Efeito de "Digitando" Adicionado

--- Scripts/index.js
+++ Scripts/index.js
@@ -25,7 +25,7 @@ const historia = {
 
   ["floresta-1"]: {
     texto:
-      "Você foi até a floresta.<br>Na floresta há várias maçãs, cuja aparência é duvidosa, espalhadas pelo chão próximas a uma árvore.<br>Além das maçãs há vários gravetos de diversos tamanhos caídos das árvores próximas, poderiam ser úteis para fabricação de ferramentas.",
+      "Você foi até a floresta. Na floresta há várias maçãs, cuja aparência é duvidosa, espalhadas pelo chão próximas a uma árvore. Além das maçãs há vários gravetos de diversos tamanhos caídos das árvores próximas, poderiam ser úteis para fabricação de ferramentas.",
     alerta: "O que você faz?",
     escolha1: "Comer <strong>maçãs</strong>",
     escolha1caminho: "maçã-1",
@@ -34,7 +34,7 @@ const historia = {
   },
   ["maçã-1"]: {
     texto:
-      "Você comeu uma das maçãs que estavam no chão.<br>Como foi dito anteriomente, elas estavam com uma aparência suspeita. Infelizmente a que você escolheu comer estava realmente podre, especialmente por dentro.",
+      "Você comeu uma das maçãs que estavam no chão. Como foi dito anteriomente, elas estavam com uma aparência suspeita. Infelizmente a que você escolheu comer estava realmente podre, especialmente por dentro.",
     alerta: "Você morreu!",
     escolha1: "Voltar ao <strong>início</strong>!",
     escolha1caminho: "início",
@@ -43,7 +43,7 @@ const historia = {
   },
   ["gravetos-1"]: {
     texto:
-      "Ao chegar perto dos gravetos, você para por alguns segundos enquanto os observa, esse pequeno tempo foi suficente para você perceber que você não fazia a menor ideia do que fazer com os gravetos após pega-los.<br>Você entra em uma crise e em uma profunda decepção consigo mesmo. Após um tempo você cai de costas no chão, bate a cabeça em uma pedra, e... pois é.",
+      "Ao chegar perto dos gravetos, você para por alguns segundos enquanto os observa, esse pequeno tempo foi suficente para você perceber que você não fazia a menor ideia do que fazer com os gravetos após pega-los. Você entra em uma crise e em uma profunda decepção consigo mesmo. Após um tempo você cai de costas no chão, bate a cabeça em uma pedra, e... pois é.",
     alerta: "Você morreu!",
     escolha1: "Voltar ao <strong>início</strong>!",
     escolha1caminho: "início",
@@ -53,7 +53,7 @@ const historia = {
 
   ["rio-1"]: {
     texto:
-      "Após uma longa caminhada você chegou a uma parte do rio, o rio é tão extenso que não é possível atravessa-lo apenas nadando, consequentemente te obrigando a ficar do lado onde está, ou talvez você possa teimar e tentar, a escolha é sua.<br>Aproveitando o rio na sua frente, você pode beber um pouco de água para matar a sua sede e quem sabe te fazer se sentir melhor.",
+      "Após uma longa caminhada você chegou a uma parte do rio, o rio é tão extenso que não é possível atravessa-lo apenas nadando, consequentemente te obrigando a ficar do lado onde está, ou talvez você possa teimar e tentar, a escolha é sua.Aproveitando o rio na sua frente, você pode beber um pouco de água para matar a sua sede e quem sabe te fazer se sentir melhor.",
     alerta: "O que você faz?",
     escolha1: "Tentar <strong>atravessar</strong> nadando",
     escolha1caminho: "nadar-1",
@@ -71,7 +71,7 @@ const historia = {
   },
   ["água-1"]: {
     texto:
-      "Você olha para o rio, após observa-lo, você imagina que não há nenhum perigo em beber alguns goles de sua água, você pega um pouco da água em suas mãos e começa a beber, você até consegue saborear e engolir aquela água, por um tempo ainda está tudo bem, você se sente despreocupado e se levanta.<br>Uau, você não morreu!<br>Ah, não, é... você começa a se sentir muito mal, começa a cambalear e infelizmente você ainda estava muito perto do rio, assim, após perder o equilibrio, você cai e consequentemente afunda na própria água ingerida por você.",
+      "Você olha para o rio, após observa-lo, você imagina que não há nenhum perigo em beber alguns goles de sua água, você pega um pouco da água em suas mãos e começa a beber, você até consegue saborear e engolir aquela água, por um tempo ainda está tudo bem, você se sente despreocupado e se levanta. Uau, você não morreu! Ah, não, é... você começa a se sentir muito mal, começa a cambalear e infelizmente você ainda estava muito perto do rio, assim, após perder o equilibrio, você cai e consequentemente afunda na própria água ingerida por você.",
     alerta: "Você morreu!",
     escolha1: "Voltar ao <strong>início</strong>!",
     escolha1caminho: "início",
@@ -80,17 +80,42 @@ const historia = {
   },
 };
 
+var texto_acabou = false;
 function changeTexts(caminho) {
-  texto.innerHTML = historia[caminho].texto;
-  escolha1.innerHTML = historia[caminho].escolha1;
-  escolha2.innerHTML = historia[caminho].escolha2;
+  texto_acabou = false;
+  var i = 0;
+  var speed = 40;
 
-  if (historia[caminho].alerta == "Você morreu!") {
-    texto_alerta.style.color = "rgb(180, 40, 68)";
-  } else {
-    texto_alerta.style.color = "rgb(29, 86, 161)";
+  texto.innerHTML = "";
+  texto_alerta.innerHTML = "";
+  escolha1.innerHTML = "Escolha 1";
+  escolha2.innerHTML = "Escolha 2";
+  texto_alerta.style.color = "rgb(0, 0, 0)";
+
+  function changeButtonsAlerts() {
+    var i2 = 0;
+    var speed2 = 20;
+
+    escolha1.innerHTML = historia[caminho].escolha1;
+    escolha2.innerHTML = historia[caminho].escolha2;
+
+    if (historia[caminho].alerta == "Você morreu!") {
+      texto_alerta.style.color = "rgb(180, 40, 68)";
+    } else {
+      texto_alerta.style.color = "rgb(29, 86, 161)";
+    }
+
+    function typeWriter2() {
+      if (i2 < historia[caminho].alerta.length) {
+        texto_alerta.innerHTML += historia[caminho].alerta.charAt(i2);
+        i2++;
+        setTimeout(typeWriter2, speed2);
+      } else {
+        texto_acabou = true;
+      }
+    }
+    typeWriter2();
   }
-  texto_alerta.innerHTML = historia[caminho].alerta;
 
   if (escolha1.hasAttribute("value")) {
     escolha1.setAttribute("value", historia[caminho].escolha1caminho);
@@ -98,14 +123,27 @@ function changeTexts(caminho) {
   if (escolha2.hasAttribute("value")) {
     escolha2.setAttribute("value", historia[caminho].escolha2caminho);
   }
+
+  function typeWriter() {
+    if (i < historia[caminho].texto.length) {
+      texto.innerHTML += historia[caminho].texto.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    } else {
+      changeButtonsAlerts();
+    }
+  }
+  typeWriter();
 }
 
 changeTexts("início");
 
-escolha1.onclick = function () {
-  changeTexts(this.getAttribute("value"));
-};
-
-escolha2.onclick = function () {
-  changeTexts(this.getAttribute("value"));
-};
+function callMainFunction(n) {
+  if (texto_acabou == true) {
+    if (n == 1) {
+      changeTexts(escolha1.getAttribute("value"));
+    } else {
+      changeTexts(escolha2.getAttribute("value"));
+    }
+  }
+}
